cc46a06e20af702064a36de6f014d262af2906d9
Michael Florendo <michael@michaels-macbook-pro.local>
2018-11-08 10:08:15 +0800

    Initial Commit

---
+++ todo-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  todo-app
+//
+//  Created by Michael Florendo on 08/11/2018.
+//  Copyright © 2018 Michael Florendo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
